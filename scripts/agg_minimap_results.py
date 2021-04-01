cc305bb905c16659a2b1b7f869a21e6dfcabb983
John Eppley <jmeppley@gmail.com>
2021-04-01 16:47:40 -0400

    cleaned up hit aggregation code

---
+++ scripts/agg_minimap_results.py
@@ -0,0 +1,13 @@
+from jme.jupy_tools.hit_tables import agg_hit_table
+
+hit_table = str(snakemake.input)
+fmt = str(params.format).upper()
+
+if os.path.getsize(hit_table) > 0:
+    agg_hit_table(hit_table, format=fmt) \
+        .to_csv(str(snakemake.output), sep='\t', index=None)
+else:
+    # input is empty, touch the output
+    with open(str(snakemake.output), 'wt') as out_handle:
+        pass
+
